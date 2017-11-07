29d6e9876c95f8ef91a179f8bc3d38cc29ac7a46
Funbit <contact@funbit.info>
2017-11-07 23:31:21 +0900

    Refactored resources, updated version to 2.0.1.7.

--- Solution/VisualHG/Resources.Designer.cs
+++ Solution/VisualHG/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace VisualHg {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,53 +57,6 @@ namespace VisualHg {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to VisualHg.
-        /// </summary>
-        internal static string _100 {
-            get {
-                return ResourceManager.GetString("100", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Provides integration with Mercurial and TortoiseHg..
-        /// </summary>
-        internal static string _101 {
-            get {
-                return ResourceManager.GetString("101", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Source Control.
-        /// </summary>
-        internal static string _102 {
-            get {
-                return ResourceManager.GetString("102", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon _400 {
-            get {
-                object obj = ResourceManager.GetObject("400", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _500 {
-            get {
-                object obj = ResourceManager.GetObject("500", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
